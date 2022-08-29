cffd7e273acf4202b1a8144841d372f79d8bded0
luqman <putraian2005@gmail.com>
2022-08-29 14:37:31 +0700

    menambahkan method get

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -9,7 +10,7 @@ import (
 // ip yang akan digunakan  "34.68.30.186:7000", "35.202.198.198:7000", "34.172.44.137:7000", "34.67.142.60:7000", "34.66.14.86:7000", "35.222.248.41:7000"
 func main() {
 	rdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{"34.68.30.186:7000", "35.202.198.198:7000", "34.172.44.137:7000", "34.67.142.60:7000", "34.66.14.86:7000", "35.222.248.41:7000"},
+		Addrs: []string{"34.68.30.186:7000", "35.202.198.198:7000", "34.172.44.137:7000", "34.67.142.60:7000", "35.222.248.41:7000"},
 
 		// To route commands by latency or randomly, enable one of the following.
 		//RouteByLatency: true,
@@ -17,11 +18,20 @@ func main() {
 	})
 	ctx := context.Background()
 
-	err := rdb.ForEachShard(ctx, func(ctx context.Context, shard *redis.Client) error {
-		return shard.Ping(ctx).Err()
+	err := rdb.ForEachMaster(ctx, func(ctx context.Context, master *redis.Client) error {
+		return master.Ping(ctx).Err()
 	})
 	if err != nil {
 		panic(err)
 	}
+	val, err := rdb.Do(ctx, "get", "key").Result()
+	if err != nil {
+		if err == redis.Nil {
+			fmt.Println("key does not exists")
+			return
+		}
+		panic(err)
+	}
+	fmt.Println(val.(string))
 
 }
