f16ad87af8efe4a9897e9e5839f8100da6b6010d
luqman <putraian2005@gmail.com>
2022-08-29 14:39:38 +0700

    mengganti ip untuk yang dideploy ke server

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 // ip yang akan digunakan  "34.68.30.186:7000", "35.202.198.198:7000", "34.172.44.137:7000", "34.67.142.60:7000", "34.66.14.86:7000", "35.222.248.41:7000"
 func main() {
 	rdb := redis.NewClusterClient(&redis.ClusterOptions{
-		Addrs: []string{"34.68.30.186:7000", "35.202.198.198:7000", "34.172.44.137:7000", "34.67.142.60:7000", "35.222.248.41:7000"},
+		Addrs: []string{"10.128.0.18:7000", "10.128.0.17:7000", "10.128.0.19:7000", "10.128.0.20:7000", "10.128.0.21:7000", "10.128.0.22:7000"},
 
 		// To route commands by latency or randomly, enable one of the following.
 		//RouteByLatency: true,
